8bd73dd65a088b15964ba2e9bbceacb52753d039
Thanuka <Thanuka.ellepola@gmail.com>
2025-05-26 20:16:11 +0530

    Add Alembic baseline migration

---
+++ migrations/versions/e084aa9fdca1_baseline_mark_current_db_as_initial.py
@@ -0,0 +1,24 @@
+"""Baseline: mark current DB as initial
+
+Revision ID: e084aa9fdca1
+Revises: 
+Create Date: 2025-05-26 20:15:55.747604
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e084aa9fdca1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
